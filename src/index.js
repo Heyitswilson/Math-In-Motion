1f601d158326f1e741ebfa92f7fd198fa929c822
Heyitswilson <wilsonngu13@gmail.com>
2020-04-22 16:30:19 -0700

    Removed extra code

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d")
     
-    let canvasPos = getPosition(canvas);
     let mouseX = 400;
     let mouseY = 400;
     
@@ -20,27 +19,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     canvas.addEventListener("mousedown", setMousePosition, false)
 
     function setMousePosition(e) {
-        mouseX = e.clientX - canvasPos.x;
-        mouseY = e.clientY - canvasPos.y;
+        mouseX = e.clientX;
+        mouseY = e.clientY;
 
         Demoview.clear()
         Demoview.moveAgain(mouseX, mouseY)
-    }
-
-    function getPosition(el) {
-        let xPosition = 0;
-        let yPosition = 0;
-
-        while (el) {
-            xPosition += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-            yPosition += (el.offsetTop - el.scrollTop + el.clientTop);
-            el = el.offsetParent
-        }
-
-        return {
-            x: xPosition,
-            y: yPosition
-        }
     }
 
 })
